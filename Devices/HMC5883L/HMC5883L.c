a4f8d8b8fbbbfa1a28f84a2acbe155e4b0656330
Aliaksandr Pachtovy <alex.mail.prime@gmail.com>
2023-08-09 23:06:19 +0300

    Edit HMC5883L library for new I2C API

--- Devices/HMC5883L/HMC5883L.c
+++ Devices/HMC5883L/HMC5883L.c
@@ -19,65 +19,36 @@
 
 #include "HMC5883L.h"
 
-static const uint32_t HMC5883L_CHIP_ID = 0x00483433; //< Assumes ALT address pin low
+#define HMC5883L_CHIP_ID  (uint32_t)0x00483433 //< Assumes ALT address pin low
+#define HMC5883L_DATA_LEN 6
 
 static inline uint16_t CONCAT_BYTES(uint8_t msb, uint8_t lsb) {
     return (uint16_t)(((uint16_t)msb << 8) | (uint16_t)lsb);
 }	
 
 uint8_t HMC5883L_Init(I2C_IRQ_Connection_t *_i2c, HMC5883L_dev *dev) {
-	if (_i2c->status == PORT_FREE)	{ //send setup
-		dev->status = DEVICE_NOT_INIT;
-		switch (dev->step) {
-		case 0: {//setup sensor
-			_i2c->addr = dev->addr;
-			FIFO_PutOne(_i2c->buffer, HMC5883L_REG_CONFIG_A);
-			_i2c->len = 3;
-			_i2c->mode = I2C_MODE_WRITE;
-			uint8_t dt[3];
-			dt[0] = HMC5883L_SAMPLES_1 | HMC5883L_DATARATE_15HZ | HMC5883L_NORMAL;
-			dt[1] = HMC5883L_GAIN_1_3GA;
-			dt[2] = HMC5883L_CONTINOUS;
-			FIFO_PutMulti(_i2c->buffer, dt, 3);
-			dev->step = 1;
-			break; }
-		case 1:
-			dev->status = DEVICE_INIT;
-			dev->step = 0;
-			return 1;
-		default:
-			dev->step = 0;
-			break;
-		}
-		I2C_Start_IRQ(_i2c);
+	dev->status = DEVICE_NOT_INIT;
+	//setup sensor
+	uint8_t data[3];
+	data[0] = HMC5883L_SAMPLES_1 | HMC5883L_DATARATE_15HZ | HMC5883L_NORMAL;
+	data[1] = HMC5883L_GAIN_1_3GA;
+	data[2] = HMC5883L_CONTINOUS;
+	if (WriteRegBytes(_i2c, dev->addr, HMC5883L_REG_CONFIG_A, data, 3)) {
+		dev->status = DEVICE_INIT;
+		dev->step = 0;
+		return 1;
 	}
 	return 0;
 }
 
 uint8_t HMC5883L_GetData(I2C_IRQ_Connection_t *_i2c, HMC5883L_dev *dev) {
-	if (_i2c->status == PORT_FREE) {
-		switch (dev->step) {
-		case 0: //read data
-			_i2c->addr = dev->addr;
-			FIFO_PutOne(_i2c->buffer, HMC5883L_REG_OUT_X_M);
-			_i2c->len = 6;
-			_i2c->mode = I2C_MODE_READ;
-			dev->step = 1;
-			break;
-		case 1: {//calculate data
-			uint8_t dt[6];
-			FIFO_GetMulti(_i2c->buffer, dt, 6);
-			dev->raw.X = CONCAT_BYTES(dt[0], dt[1]);
-			dev->raw.Z = CONCAT_BYTES(dt[2], dt[3]);
-			dev->raw.Y = CONCAT_BYTES(dt[4], dt[5]);
-			dev->status = DEVICE_DONE;
-			dev->step = 0;
-			return 1; }
-		default:
-			dev->step = 0;
-			break;
-		}
-		I2C_Start_IRQ(_i2c);
+	uint8_t data[HMC5883L_DATA_LEN];
+	if (ReadRegBytes(_i2c, dev->addr, HMC5883L_REG_OUT_X_M, data, HMC5883L_DATA_LEN)) {
+		dev->raw.X = CONCAT_BYTES(data[0], data[1]);
+		dev->raw.Z = CONCAT_BYTES(data[2], data[3]);
+		dev->raw.Y = CONCAT_BYTES(data[4], data[5]);
+		dev->status = DEVICE_DONE;
+		return 1;
 	}
 	return 0;
 }

440a2a61a1dce028844d75a6811e1668af817700
Aliaksandr Pachtovy <alex.mail.prime@gmail.com>
2023-08-13 22:38:13 +0300

    Test I2C commit

--- MCU/F1xx/Peripherals/I2C/MyI2C.c
+++ MCU/F1xx/Peripherals/I2C/MyI2C.c
@@ -21,7 +21,7 @@
 /*запускает обмен с I2C через прерывания, использовать
  * после того как передали в кольцевой буфер все необходимые данные
  */
-void I2C_Start_IRQ(I2C_IRQ_Connection_t *_i2c) {
+void I2C_Start_IRQ(I2C_IRQ_Conn_t *_i2c) {
 	_i2c->status = PORT_BUSY;
 	LL_I2C_EnableIT_BUF(_i2c->i2c);	//Enable TXE RxNE iterrupt for >1 byte
 	if (_i2c->len > 1) {
@@ -33,22 +33,25 @@ void I2C_Start_IRQ(I2C_IRQ_Connection_t *_i2c) {
 	LL_I2C_GenerateStartCondition(_i2c->i2c);
 }
 /*запускает обмен по I2C с использованием DMA, не дописано*/
-void I2C_Start_DMA(I2C_DMA_Connection_t *_i2c) {
+void I2C_Start_DMA(I2C_DMA_Conn_t *_i2c) {
 	_i2c->status = PORT_BUSY;
-	LL_I2C_DisableDMAReq_TX(_i2c->i2c);
-	LL_I2C_DisableIT_BUF(_i2c->i2c);//отключаем прерывания чтобы работало DMA
+	LL_I2C_DisableDMAReq_TX(_i2c->i2c);	//disable DMA req while setting
+	LL_I2C_DisableIT_BUF(_i2c->i2c);		//disable interrupt for DMA work
 	if (_i2c->len > 1) {
 		LL_I2C_AcknowledgeNextData(_i2c->i2c, LL_I2C_ACK); 	// Ack enable if more one bytes read
 	}	else {
 		LL_I2C_AcknowledgeNextData(_i2c->i2c, LL_I2C_NACK);//Ack disable if only one byte read
 	}
-	//DMA_Ch4_Restart(DMA1, _i2c->buffer, (uint16_t)_i2c->len);
+	LL_DMA_DisableChannel((DMA_TypeDef*)_i2c->DMAx, _i2c->Channel);
+	LL_DMA_SetMemoryAddress((DMA_TypeDef*)_i2c->DMAx, _i2c->Channel, (uint32_t)_i2c->buffer);
+	LL_DMA_SetDataLength((DMA_TypeDef*)_i2c->DMAx, _i2c->Channel, (uint32_t)_i2c->len);
+	LL_DMA_EnableChannel((DMA_TypeDef*)_i2c->DMAx, _i2c->Channel);
 	LL_I2C_GenerateStartCondition(_i2c->i2c);
 }
 /* альтернативный основной обработчик прерывания, использовать в блоке обработки прерываний
  * после окончания обработки присваивает соединению статус "свободно"
  * при ошибках будет выполняться обработчик ошибок*/
-void I2C_Raw_IRQ_CallBack(I2C_IRQ_Connection_t *_i2c) {
+void I2C_Raw_IRQ_CallBack(I2C_IRQ_Conn_t *_i2c) {
 	//_i2c->status = PORT_BUSY;
 	volatile uint16_t I2C_SR1 = LL_I2C_ReadReg(_i2c->i2c, SR1);//Read SR1 first
 	//EV5 Start condition generated. Clear: read SR1 and write slave addr to DR final
@@ -134,7 +137,7 @@ void I2C_Raw_IRQ_CallBack(I2C_IRQ_Connection_t *_i2c) {
 }
 
 /* обработчик прерываний при обмене с использованием DMA переделать*/
-void I2C_EV_IRQ_DMA_CallBack(I2C_DMA_Connection_t *_i2c) {
+void I2C_EV_IRQ_DMA_CallBack(I2C_DMA_Conn_t *_i2c) {
 	volatile uint16_t I2C_SR1 = LL_I2C_ReadReg(_i2c->i2c, SR1);	//Read SR1
 	//EV5 Start condition generated. Clear: read SR1 and write slave addr to DR
 	if (I2C_SR1 & I2C_SR1_SB) {//send device address
@@ -150,7 +153,7 @@ void I2C_EV_IRQ_DMA_CallBack(I2C_DMA_Connection_t *_i2c) {
 	else if (I2C_SR1 & I2C_SR1_ADDR) {// slave found and ASK recive
 		if (_i2c->mode == I2C_MODE_WRITE) {//work correct
 			(void) _i2c->i2c->SR2;	//Read SR2, clear ADDR
-			FIFO_GetOne(_i2c->buffer, ((uint8_t *)&_i2c->i2c->DR));//It's FIRST byte			//Send first byte reg address
+			_i2c->i2c->DR = _i2c->reg;//It's FIRST byte			//Send first byte reg address
 			LL_I2C_EnableDMAReq_TX(_i2c->i2c);//i2c_conn->I2Cbus->CR2 |= I2C_CR2_DMAEN;
 		}
 		return;
@@ -163,7 +166,7 @@ void I2C_EV_IRQ_DMA_CallBack(I2C_DMA_Connection_t *_i2c) {
 	}
 }
 //=============================================================================
-void I2C_ERR_IRQ_CallBack(I2C_IRQ_Connection_t *_i2c) {
+void I2C_ERR_IRQ_CallBack(I2C_IRQ_Conn_t *_i2c) {
 	volatile uint32_t I2C_SR1 = 0;
 	volatile uint32_t I2C_SR2 = 0;
 	_i2c->status = PORT_ERROR;
@@ -208,6 +211,52 @@ void I2C_ERR_IRQ_CallBack(I2C_IRQ_Connection_t *_i2c) {
 	_i2c->status = PORT_FREE;
 }
 //=============================================================================
+
+void I2C_ERR_DMA_CallBack(I2C_IRQ_Conn_t *_i2c) {
+	volatile uint32_t I2C_SR1 = 0;
+	volatile uint32_t I2C_SR2 = 0;
+	_i2c->status = PORT_ERROR;
+	I2C_SR1 = _i2c->i2c->SR1;/* Read the I2C1 status register */
+	I2C_SR2 = _i2c->i2c->SR2;/* Read the I2C1 status register */
+	/* 10 bit AF detect */
+	if (I2C_SR1 & I2C_SR1_AF) {//+
+		LL_I2C_ClearFlag_AF(_i2c->i2c);
+	}
+		/* 8 bit BERR detect start-stop faulth, full restart bus*/
+	else if (I2C_SR1 & I2C_SR1_BERR) {//+
+		LL_I2C_ClearFlag_BERR(_i2c->i2c);
+	}
+	/* 9 bit ARLO detect wrong master found */
+	else if (I2C_SR1 & I2C_SR1_ARLO) {//+
+		LL_I2C_ClearFlag_ARLO(_i2c->i2c);
+	}
+	/* 11 bit OVR detect input buffer overload */
+	else if (I2C_SR1 & I2C_SR1_OVR) {//+
+		LL_I2C_ClearFlag_OVR(_i2c->i2c);
+	}
+	/* 12 bit PECERR detect */
+	else if (I2C_SR1 & I2C_SR1_PECERR) {//+
+		LL_I2C_ClearSMBusFlag_PECERR(_i2c->i2c);
+	}
+	/* 14 bit TIMEOUT detect */
+	else if (I2C_SR1 & I2C_SR1_TIMEOUT) {//+
+		LL_I2C_ClearSMBusFlag_TIMEOUT(_i2c->i2c);
+	}
+	/* 15 bit SMBALERT detect */
+	else if (I2C_SR1 & I2C_SR1_SMBALERT) {//+
+		LL_I2C_ClearSMBusFlag_ALERT(_i2c->i2c);
+	}
+	else if (I2C_SR2 & I2C_SR2_BUSY) {
+		ClearBusyI2C1();
+	}
+	else if (I2C_SR1 & I2C_SR1_STOPF) {//+
+		LL_I2C_Disable(I2C1);
+		LL_I2C_Enable(I2C1);
+		}
+	LL_I2C_GenerateStopCondition(_i2c->i2c);
+	_i2c->status = PORT_FREE;
+}
+
 void ClearBusyI2C1(void){
 	volatile static uint8_t step = 0;
 	//I2C1 GPIO Configuration
@@ -255,7 +304,7 @@ void ClearBusyI2C1(void){
   * @retval
   * 0 - processing, 1 - complite
   */
-uint8_t WriteOneRegByte(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, uint8_t value) {
+uint8_t WriteOneRegByte(I2C_IRQ_Conn_t *_i2c, uint8_t addr, uint8_t reg, uint8_t value) {
 	if (_i2c->status == PORT_FREE) {
 		switch (_i2c->step) {
 		case 0:  // write data
@@ -285,7 +334,7 @@ uint8_t WriteOneRegByte(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, u
   * @retval
   * 0 - processing, 1 - complite
   */
-uint8_t WriteRegBytes(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, void *data, uint8_t size) {
+uint8_t WriteRegBytes(I2C_IRQ_Conn_t *_i2c, uint8_t addr, uint8_t reg, void *data, uint8_t size) {
 	if (_i2c->status == PORT_FREE) {
 		switch (_i2c->step) {
 		case 0:  // write data
@@ -312,7 +361,7 @@ uint8_t WriteRegBytes(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, voi
   * @param
   * @retval
   */
-uint8_t ReadOneRegByte(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, uint8_t *value) {
+uint8_t ReadOneRegByte(I2C_IRQ_Conn_t *_i2c, uint8_t addr, uint8_t reg, uint8_t *value) {
 	if (_i2c->status == PORT_FREE) {
 		switch (_i2c->step) {
 		case 0:  // read data
@@ -339,7 +388,7 @@ uint8_t ReadOneRegByte(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, ui
   * @param
   * @retval
   */
-uint8_t ReadRegBytes(I2C_IRQ_Connection_t *_i2c, uint8_t addr, uint8_t reg, void *data, uint8_t size) {
+uint8_t ReadRegBytes(I2C_IRQ_Conn_t *_i2c, uint8_t addr, uint8_t reg, void *data, uint8_t size) {
 	if (_i2c->status == PORT_FREE) {
 		switch (_i2c->step) {
 		case 0:  // read data
